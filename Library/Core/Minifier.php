93d64de2a71703ebabf522560be01b934fd77724
terriani <viniterriani.vt@gmail.com>
2019-10-22 19:50:44 -0300

    correçao do algoritmo de minificaçao

--- Library/Core/Minifier.php
+++ Library/Core/Minifier.php
@@ -8,27 +8,29 @@ if ($_SERVER['SERVER_NAME'] == 'localhost' or $minify) {
      * Minify Css
      */
     $minifierCss = new Minify\CSS();
-    $cssDir = scandir(dirname(__DIR__, 1)."/Public/assets/css/");
+    $cssDir = scandir(dirname(__DIR__, 2)."/Public/assets/css/");
     foreach ($cssDir as $cssItem) {
-        $cssFile = dirname(__DIR__, 1)."/Public/assets/css/$cssItem";
+        $cssFile = dirname(__DIR__, 2)."/Public/assets/css/$cssItem";
         if (is_file($cssFile) and pathinfo($cssFile)['extension'] == "css") {
+            @unlink(dirname(__DIR__, 2)."/Public/assets/css/scooby.min.css");
             $minifierCss->add($cssFile);
         }
     }
-    $minifiedPath = dirname(__DIR__, 1)."/Public/assets/css/scooby.min.css";
+    $minifiedPath = dirname(__DIR__, 2)."/Public/assets/css/scooby.min.css";
     $minifierCss->minify($minifiedPath);
 
     /**
      * Minify js
      */
     $minifierJs = new Minify\JS();
-    $jsDir = scandir(dirname(__DIR__, 1)."/Public/assets/js/");
+    $jsDir = scandir(dirname(__DIR__, 2)."/Public/assets/js/");
     foreach ($jsDir as $jsItem) {
-        $jsFile = dirname(__DIR__, 1)."/Public/assets/js/$jsItem";
+        $jsFile = dirname(__DIR__, 2)."/Public/assets/js/$jsItem";
         if (is_file($jsFile) and pathinfo($jsFile)['extension'] == "js") {
+            @unlink(dirname(__DIR__, 2)."/Public/assets/js/scooby.min.js");
             $minifierJs->add($jsFile);
         }
     }
-    $minifiedPath = dirname(__DIR__, 1)."/Public/assets/js/scooby.min.js";
+    $minifiedPath = dirname(__DIR__, 2)."/Public/assets/js/scooby.min.js";
     $minifierJs->minify($minifiedPath);
 }
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Helpers\Csrf;
 session_start();
 require_once 'vendor/autoload.php';
 require_once 'Config/config.php';
-require_once 'Config/minifier.php';
+require_once 'Library/Core/Minifier.php';
 require_once 'Config/Routers.php';
 Csrf::csrfTokengenerate();
 $c = new Core\Core;
