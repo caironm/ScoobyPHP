687661d4a6eed82c2c054f0254b55cb4a86192cf
terriani <viniterriani.vt@gmail.com>
2020-01-14 03:24:17 -0300

    corrigindo o cli do framework

--- System/Helpers/Cli.php
+++ System/Helpers/Cli.php
@@ -15,7 +15,7 @@ class Cli
      */
     public static function println(string $msg): void
     {
-        echo $msg.PHP_EOL;
+        echo "  ".$msg.PHP_EOL;
     }
 
     /**
@@ -26,7 +26,7 @@ class Cli
      */
     public static function getParam(string $msg): string
     {
-        echo $msg.PHP_EOL;
+        echo "  ".$msg.PHP_EOL."\033[1;32m -> \033[1;97m";
         $param = fgets(STDIN);
         $param = trim($param);
         return $param;
