cdfad6e15119d27ec92a5eaae9e579b9839039df
vinicius <vinicius.terriani@jestor.com.br>
2020-04-30 12:09:58 -0300

    comentario no helper httpClient

--- System/Helpers/HttpClient.php
+++ System/Helpers/HttpClient.php
@@ -6,7 +6,20 @@ use Zttp\Zttp;
 
 class HttpClient
 {
-    public static function get(string $url, array $param = [], array $headers = [], array $options = [])
+    /**
+     * Efetua uma requisição do tipo GET para a URL informada via parametros e retorna um objeto com os dados de retorno
+     * Além da URL pode-se passar também três arrays,
+     * O primeiro é um array associativo para envio de parametros para a url, ficando por exemplo -> ['foo' => 'bar'], o envio de parametros é ilimitado
+     * O segundo é um array associativo para envio de headers para a url, ficando por exemplo -> ['token' => 'abc123'], o envio de headers é ilimitado
+     * O Terceiro é um array associativo para envio de options para a url, ficando por exemplo -> ['accept' => 'text/xml'], o envio de options é ilimitado
+     *
+     * @param string $url
+     * @param array $param
+     * @param array $headers
+     * @param array $options
+     * @return object
+     */
+    public static function get(string $url, array $param = [], array $headers = [], array $options = []): object
     {
         $response = Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->get($url, $param);
         return (object) [
@@ -19,7 +32,20 @@ class HttpClient
         ];
     }
 
-    public static function post(string $url, array $param = [], array $headers = [], array $options = [])
+    /**
+     * Efetua uma requisição do tipo POST para a URL informada via parametros e retorna um objeto com os dados de retorno
+     * Além da URL pode-se passar também três arrays,
+     * O primeiro é um array associativo para envio de parametros para a url, ficando por exemplo -> ['foo' => 'bar'], o envio de parametros é ilimitado
+     * O segundo é um array associativo para envio de headers para a url, ficando por exemplo -> ['token' => 'abc123'], o envio de headers é ilimitado
+     * O Terceiro é um array associativo para envio de options para a url, ficando por exemplo -> ['accept' => 'text/xml'], o envio de options é ilimitado
+     *
+     * @param string $url
+     * @param array $param
+     * @param array $headers
+     * @param array $options
+     * @return object
+     */
+    public static function post(string $url, array $param = [], array $headers = [], array $options = []): object
     {
         $response = Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->post($url, $param);
         return (object) [
@@ -32,7 +58,20 @@ class HttpClient
         ];
     }
 
-    public static function put(string $url, array $param = [], array $headers = [], array $options = [])
+    /**
+     * Efetua uma requisição do tipo PUT para a URL informada via parametros e retorna um objeto com os dados de retorno
+     * Além da URL pode-se passar também três arrays,
+     * O primeiro é um array associativo para envio de parametros para a url, ficando por exemplo -> ['foo' => 'bar'], o envio de parametros é ilimitado
+     * O segundo é um array associativo para envio de headers para a url, ficando por exemplo -> ['token' => 'abc123'], o envio de headers é ilimitado
+     * O Terceiro é um array associativo para envio de options para a url, ficando por exemplo -> ['accept' => 'text/xml'], o envio de options é ilimitado
+     *
+     * @param string $url
+     * @param array $param
+     * @param array $headers
+     * @param array $options
+     * @return object
+     */
+    public static function put(string $url, array $param = [], array $headers = [], array $options = []): object
     {
         $response = Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->put($url, $param);
         return (object) [
@@ -45,7 +84,20 @@ class HttpClient
         ];
     }
 
-    public static function delete(string $url, array $param = [], array $headers = [], array $options = [])
+    /**
+     * Efetua uma requisição do tipo DELETE para a URL informada via parametros e retorna um objeto com os dados de retorno
+     * Além da URL pode-se passar também três arrays,
+     * O primeiro é um array associativo para envio de parametros para a url, ficando por exemplo -> ['foo' => 'bar'], o envio de parametros é ilimitado
+     * O segundo é um array associativo para envio de headers para a url, ficando por exemplo -> ['token' => 'abc123'], o envio de headers é ilimitado
+     * O Terceiro é um array associativo para envio de options para a url, ficando por exemplo -> ['accept' => 'text/xml'], o envio de options é ilimitado
+     *
+     * @param string $url
+     * @param array $param
+     * @param array $headers
+     * @param array $options
+     * @return object
+     */
+    public static function delete(string $url, array $param = [], array $headers = [], array $options = []): object
     {
         $response = Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->delete($url, $param);
         return (object) [
