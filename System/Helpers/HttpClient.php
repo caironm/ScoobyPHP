ba7cf4c18acfeda447ce7f2b0d14dee47cfd005b
vinicius <vinicius.terriani@jestor.com.br>
2020-04-29 23:08:49 -0300

    refatoração do httpClient

--- System/Helpers/HttpClient.php
+++ System/Helpers/HttpClient.php
@@ -8,49 +8,53 @@ class HttpClient
 {
     public static function get(string $url, array $param = [], array $headers = [], array $options = [])
     {
+        $response = Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->get($url, $param);
         return (object) [
-            'headers' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->get($url, ['headers' => ['content-type' => ['application/x-www-form-urlencoded']]], $param)->headers(),
-            'body' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->get($url, $param)->json(),
-            'ok' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->get($url, $param)->isSuccess(),
-            'status' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->get($url, $param)->status(),
-            'serverError' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->get($url, $param)->isServerError(),
-            'clientError' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->get($url, $param)->isClientError(),
+            'headers' => $response->headers(),
+            'body' => $response->json(),
+            'ok' => $response->isSuccess(),
+            'status' => $response->status(),
+            'serverError' => $response->isServerError(),
+            'clientError' => $response->isClientError(),
         ];
     }
 
     public static function post(string $url, array $param = [], array $headers = [], array $options = [])
     {
+        $response = Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->post($url, $param);
         return (object) [
-            'headers' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->post($url, ['headers' => ['content-type' => ['application/x-www-form-urlencoded']]], $param)->headers(),
-            'body' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->post($url, $param)->json(),
-            'ok' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->post($url, $param)->isSuccess(),
-            'status' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->post($url, $param)->status(),
-            'serverError' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->post($url, $param)->isServerError(),
-            'clientError' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->post($url, $param)->isClientError(),
+            'headers' => $response->headers(),
+            'body' => $response->json(),
+            'ok' => $response->isSuccess(),
+            'status' => $response->status(),
+            'serverError' => $response->isServerError(),
+            'clientError' => $response->isClientError(),
         ];
     }
 
     public static function put(string $url, array $param = [], array $headers = [], array $options = [])
     {
+        $response = Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->put($url, $param);
         return (object) [
-            'headers' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->put($url, ['headers' => ['content-type' => ['application/x-www-form-urlencoded']]], $param)->headers(),
-            'body' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->put($url, $param)->json(),
-            'ok' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->put($url, $param)->isSuccess(),
-            'status' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->put($url, $param)->status(),
-            'serverError' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->put($url, $param)->isServerError(),
-            'clientError' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->put($url, $param)->isClientError(),
+            'headers' => $response->headers(),
+            'body' => $response->json(),
+            'ok' => $response->isSuccess(),
+            'status' => $response->status(),
+            'serverError' => $response->isServerError(),
+            'clientError' => $response->isClientError(),
         ];
     }
 
     public static function delete(string $url, array $param = [], array $headers = [], array $options = [])
     {
+        $response = Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->delete($url, $param);
         return (object) [
-            'headers' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->delete($url, ['headers' => ['content-type' => ['application/x-www-form-urlencoded']]], $param)->headers(),
-            'body' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->delete($url, $param)->json(),
-            'ok' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->delete($url, $param)->isSuccess(),
-            'status' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->delete($url, $param)->status(),
-            'serverError' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->delete($url, $param)->isServerError(),
-            'clientError' => Zttp::withOptions($options)->withHeaders($headers)->asMultipart()->asFormParams()->delete($url, $param)->isClientError(),
+            'headers' => $response->headers(),
+            'body' => $response->json(),
+            'ok' => $response->isSuccess(),
+            'status' => $response->status(),
+            'serverError' => $response->isServerError(),
+            'clientError' => $response->isClientError(),
         ];
     }
 }
